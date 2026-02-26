e170cbeaa7940adfbf58b69dd4829ac0b809818f
lusii <lusijannam@gmail.com>
2026-02-27 01:12:13 +0800

    repo

--- src/Repository.java
+++ src/Repository.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Repository {
 
-    private final String DB_URL = "jdbc:mysql:/localhost:3306/cc3_task3?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Manila";
+    private final String DB_URL = "jdbc:mysql://localhost:3306/cc3_task3?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Manila";
     private final String USER = "root";
     private final String PASS = "kyhoon0808";
     private Connection conn;
